779be8c75206008a2e1afff1ace21da6cdbcf52d
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-08 02:21:16 +0500

    work complete

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -53,7 +53,7 @@ const ProductList = () => {
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products:</FilterText>
-          <Select  onChange={handleFilter}>
+          <Select name="color" onChange={handleFilter}>
             <Option disabled >
               Color
             </Option>
@@ -64,7 +64,7 @@ const ProductList = () => {
             <Option>Yellow</Option>
             <Option>Green</Option>
           </Select>
-          <Select onChange={handleFilter}>
+          <Select name="size" onChange={handleFilter}>
             <Option disabled >
               Size
             </Option>
