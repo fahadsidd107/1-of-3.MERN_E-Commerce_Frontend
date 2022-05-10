ca96b51f636391f942ce586b64b0c8089afbfd03
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-11 00:16:44 +0500

    work complete

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,42 +1,69 @@
-import React, { useState,useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import {popularProducts} from "../Data";
+// import { popularProducts } from "../data";
 import Product from "./Product";
 import axios from "axios";
 
 const Container = styled.div`
-padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = ({cat,filter,sort}) => {
+const Products = ({ cat, filters, sort }) => {
   const [products, setProducts] = useState([]);
-  const [filteredproducts, setFilteredProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:7777/api/product?category=${cat}`
+            : "http://localhost:7777/api/product"
+        );
+        setProducts(res.data);
+        console.log(setProducts(res.data))
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
 
-    const getProducts= async ()=>{
-try{
- const res = await axios.get( cat ? `http://localhost:7777/api/product?category=${cat}` : `http://localhost:7777/api/product`);
- setProducts(res.data)
-}catch(err){}
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
     }
-    getProducts()
-  },[cat]);
+  }, [sort]);
 
-  useEffect(() => {
-    cat && setFilteredProducts(
-      products.filter((item)=> Object.entries(filter).every(([key,value])=>item[key].includes(value)))
-    );
-  } , [products,cat,filter]);
-  
   return (
     <Container>
-      {filteredproducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
