ae3430a4a49d7d4c801338b90419e19270ce7753
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-20 16:24:53 +0500

    work complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import { Badge } from "@material-ui/core";
-import {mobile} from "./../Responsive";
-import {useSelector} from "react-redux";
+import { mobile } from "./../Responsive";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   height: 60px;
-  ${mobile({ width: "50px" })}  
+  ${mobile({ width: "50px" })}
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
@@ -43,28 +43,28 @@ const Input = styled.input`
 `;
 
 const Center = styled.div`
-flex: 1;
-text-align: center;
+  flex: 1;
+  text-align: center;
 `;
 
 const Logo = styled.h1`
-font-weight: bold;
-${mobile({ fontSize: "24px" })}
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-    justify-content: flex-end;
-    ${mobile({ flex: 2, justifyContent: "center" })}
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
-font-size:14px;
-cursor:pointer;
-margin-left: 25px;
-${mobile({ fontSize: "12px", marginLeft: "10px" })}
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 function Navbar() {
@@ -77,17 +77,17 @@ function Navbar() {
           <Language>EN</Language>
           <SearchContainer>
             <Input placeholder="Search" />
-            <Search style={{ color: "gray", fontSize: 16 }}/>
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
-            <Logo>FAHAD.</Logo>
+          <Logo>FAHAD.</Logo>
         </Center>
         <Right>
-        <MenuItem>REGISTER</MenuItem>
+          <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
           <MenuItem>
-          <Badge badgeContent={4} color="primary">
+            <Badge badgeContent={4} color="primary">
               <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
