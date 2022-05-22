3c170934c20c683a7becac33daab940735aa73ed
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-22 18:32:29 +0500

    work complete

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -148,6 +148,7 @@ const Product = () => {
 
   const handleClick = () => {
     dispatch(addProduct({ ...product, quantity, color, size }));
+    console.log(product);
   };
   return (
     <Container>
