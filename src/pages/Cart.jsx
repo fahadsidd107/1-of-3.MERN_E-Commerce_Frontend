6e96650110561ff4928d2e4434e50825dc5f1a92
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-25 02:55:49 +0500

    work complete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 import StripeCheckout from 'react-stripe-checkout';
 import { useEffect, useState } from "react";
 import { userRequest } from "../requestMethods";
-import { useHistory } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 const KEY=process.env.REACT_STRIPE_KEY
 
@@ -162,7 +162,7 @@ const Button = styled.button`
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const [stripeToken,setStripeToken] = useState(null)
-  const history = useHistory();
+  const navigate = useNavigate();
 
 
   const onToken = (token) => {
@@ -176,13 +176,13 @@ const Cart = () => {
           tokenId: stripeToken.id,
           amount: 500,
         });
-        history.push("/success", {
+        navigate("/success", {
           stripeData: res.data,
           products: cart, });
       } catch {}
     };
     stripeToken && makeRequest();
-  }, [stripeToken, cart.total, history]);
+  }, [stripeToken, cart.total, navigate]);
 
   return (
     <Container>
