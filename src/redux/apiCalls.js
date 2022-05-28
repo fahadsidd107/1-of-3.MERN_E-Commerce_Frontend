da1769c9b597eb4057bdeaf85b5e163068c233e1
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-28 18:46:33 +0500

    work complete

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,10 +1,11 @@
-import { loginStart,loginFailure } from "./userRedux";
-import { userRequest } from ",,/requestMethods";
+import { loginStart,loginFailure,loginSuccess } from "./userRedux";
+import { userRequest } from "../requestMethods";
 
 export const login = async (dispatch,user)=>{
     dispatch(loginStart());
     try{
         const res = await userRequest.post("/auth/login",user);
+        dispatch(loginSuccess(res.data));
     }catch(err){
         dispatch(loginFailure());
     }
