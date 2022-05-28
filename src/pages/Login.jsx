8b69b31e068bd3bc0796d1d9a1ba01a6352a4180
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-28 18:48:15 +0500

    work complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from "styled-components";
 import { mobile } from './../Responsive'
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
   width: 100vw;
