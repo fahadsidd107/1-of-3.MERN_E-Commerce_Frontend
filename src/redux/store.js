07f8cd5d91d389e7339a47fccdd619b840f6709b
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-30 16:53:34 +0500

    work complete

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,6 +13,14 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
 export default configureStore({
   reducer: {
     cart: cartReducer,
