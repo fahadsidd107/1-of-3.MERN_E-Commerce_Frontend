a2cd46bbc48a5a53c8f89445ae2e1153f322142d
fahadsidd107 <fsiddiqui107@gmail.com>
2022-06-09 18:04:44 +0500

    work complete

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:7777/api/"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjMwYzdkY2Q0NGJmYjc1MDg2MmJmNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MjQ3NDU5MiwiZXhwIjoxNjUyNzMzNzkyfQ.kMpE4mPcHOQGxD2nTZFKNk39CHPFNhwskADg692uQ9s"
+const TOKEN = ""
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
